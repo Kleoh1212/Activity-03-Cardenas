059b579f608c9f05577febf0b2342bd2ede1cd7e
Abigail Cardenas <abigail.cardenas.edu.ph>
2022-04-28 23:52:47 +0800

    Message

--- Cardenas_script_1_stat.py
+++ Cardenas_script_1_stat.py
@@ -1,1 +1,9 @@
+from logging import _Level
+
+
 class pokestats():
+    def statReturnHP(base,iv,ev,Level):
+        return ((((2*base)+iv+(ev/4))*Level)/100)+Level+10
+
+    def statReturnOtherStat(base,iv,ev,Level,nature):
+        return((((2*base)+iv+(ev/4))*Level/100)+5)*nature
